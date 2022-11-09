43a6a5a52b1c92c32c0df8532bb46790f4d7e316
den-rgb <den02_m@yahoo.com>
2022-11-09 10:21:06 +0000

    Created and lnked MainApp

--- app/src/main/java/com/example/pacgamesandroid/activities/MainActivity.kt
+++ app/src/main/java/com/example/pacgamesandroid/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pacgamesandroid.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pacgamesandroid.databinding.ActivityMainBinding
+import com.example.pacgamesandroid.main.MainApp
 import com.example.pacgamesandroid.models.GameModel
 import com.google.android.material.snackbar.Snackbar
 import timber.log.Timber
@@ -11,31 +12,28 @@ import timber.log.Timber.i
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var game = GameModel()
-    val games = ArrayList<GameModel>()
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Timber.plant(Timber.DebugTree())
-
-        Timber.i("Game Activity started..")
-
+        app = application as MainApp
+        i("Game Activity started...")
         binding.btnAdd.setOnClickListener() {
-            game.title= binding.gameTitle.text.toString()
+            game.title = binding.gameTitle.text.toString()
             game.description = binding.description.text.toString()
             if (game.title.isNotEmpty()) {
-                games.add(game.copy())
+                app.games.add(game.copy())
                 i("add Button Pressed: $game")
-                for (i in games.indices)
-                { i("Game[$i]:${this.games[i]}") }
-            }else{
-                Snackbar
-                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                for (i in app.games.indices)
+                { i("Game[$i]:${app.games[i]}") }
+            }
+            else {
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
-
     }
 }
